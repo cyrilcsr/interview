7411cd8c195a0440c00fd217cfb2e087237cb168
Shirui Cheng <shiruic@outlook.com>
2022-02-24 15:49:04 +0800

    JianZhi 2-024 ReverseList

--- src/main/java/com/algorithms/jianZhi/ReverseList.java
+++ src/main/java/com/algorithms/jianZhi/ReverseList.java
@@ -1,7 +1,18 @@
 package com.algorithms.jianZhi;
 
+/*
+JianZhi Offer 2-024
+Leeetcode 0206
+ */
 public class ReverseList {
     public ListNode reverseList(ListNode head) {
-
+        ListNode prev = null;
+        while(head!=null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
