14f463c43ed9781af387aabb01404aca2f314fb8
Shirui Cheng <shiruic@outlook.com>
2022-03-03 14:35:47 +0800

    Leeetcode 0006 ZigZagConversion

--- src/main/java/com/algorithms/leetcode/ZigZagConversion.java
+++ src/main/java/com/algorithms/leetcode/ZigZagConversion.java
@@ -7,14 +7,22 @@ Leeetcode 0006
  */
 public class ZigZagConversion {
     public String convert(String s, int numRows) {
-        ArrayList<StringBuilder> results = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        char[] inputString = s.toCharArray();
+        int len = s.length();
+        if(numRows==1||len==1){
+            return s;
+        }
+        int period = 2*numRows-2;
         for(int i=0;i<numRows;i++){
-            results.add(new StringBuilder());
+            for(int j=0;j+i<len;j+=period){
+                sb.append(inputString[j+i]);
+                if(0<i&&i<numRows-1&&j+period-i<len){
+                    sb.append(inputString[j+period-i]);
+                }
+            }
         }
-        int index = 0;
-        for(char c:s.toCharArray()){
-            results.get(index).append(c);
-            if()
-        }
+
+        return sb.toString();
     }
 }
